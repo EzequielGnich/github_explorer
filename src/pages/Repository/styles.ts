782a21c585b9ec5092a5191b44c5e703846b2dcb
Ezequiel Gnich <ezequielgnich@live.com>
2020-12-13 16:47:41 -0300

    first commit

---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,136 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+
+    &:hover {
+      color: #666666;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+export const Loading = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    & + a {
+      margin-top: 10px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
+
+  div {
+    margin: 0 16px;
+    flex: 1;
+
+    strong {
+      font-size: 20px;
+      color: #3d3d4d;
+    }
+
+    p {
+      font-size: 16px;
+      color: #a8a8b3;
+      margin-top: 4px;
+    }
+  }
+
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
+`;
